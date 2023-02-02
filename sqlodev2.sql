a5e1394f82971ab776890cfec17186842e054318
tasoyarar <tasoyarar452@gmail.com>
2023-02-02 17:50:37 +0300

    first commit

---
+++ sqlodev2.sql
@@ -0,0 +1,3 @@
+--SELECT * FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.99;
+--select first_name, last_name from actor where first_name in ('Penelope','Nick','Ed'); 
+--select * from film where rental_rate in (0.99, 2.99, 4.99) and replacement_cost in (12.99, 15.99, 28.99);
